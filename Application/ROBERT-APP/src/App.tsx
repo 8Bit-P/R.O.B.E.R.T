c071e45fb8c1f5c2366c3714d106fa37b11eec4e
Pablo Pérez Martín <aperezvic21@gmail.com>
2024-11-01 17:14:44 +0100

    Card UI in React

--- Application/ROBERT-APP/src/App.tsx
+++ Application/ROBERT-APP/src/App.tsx
@@ -1,15 +1,14 @@
 import "./App.css";
+import Card from "./components/Card";
+import Connection from "./components/Cards/Connection";
 
 function App() {
-
-
   return (
     <main className="container">
-      <h1>Welcome to Tauri + React</h1>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+
+      <Card width={200} height={200} title={"Joints"} children={<Connection/>}/>
+      <Card width={300} height={150} title={"Connection"} children={<Connection/>}/>
     </main>
   );
 }
