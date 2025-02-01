3f9a4a6486887a65cfcf461b859825a1aa354386
Pablo Pérez Martín <aperezvic21@gmail.com>
2025-02-01 14:50:54 +0100

    Defined pins

--- ArduinoScripts/SerialController/Constants.h
+++ ArduinoScripts/SerialController/Constants.h
@@ -21,8 +21,8 @@
 
 //Error codes
 #define CommandFormatError "C001" // Command was not properly formated
-#define CommandNotDefined "C002"
-#define InvalidStepper "I001"
+#define CommandNotDefined "C002" // Given command is not defined
+#define InvalidStepper "I001" // Invalid stepper selected
 #define InvalidState "I002" //Invalid state for stepper
 
 //Ranges
@@ -30,38 +30,34 @@
 #define VelocityUpperRange = 2000     //TODO: check actual upper ranges
 
 // Define stepper motor connections and motor interface type. 
-//TODO: map pins correctly
-#define J1stepPin 26 //E0-STEP
-#define J1dirPin 28 // E0-DIR 
-#define J1enablePin 24 // E0-EN
+#define J1stepPin 45 //TB6600 STEP
+#define J1dirPin 47 //TB6600 DIR
+#define J1enablePin 32 //TB6600 ENABLE
+#define J1limitPin 19  //Z-MAX
 
-#define J2stepPin 36 //E1-STEP
-#define J2dirPin 34 //E1-DIR
-#define J2enablePin 30 // E1-EN
+#define J2stepPin 26 //E0-STEP
+#define J2dirPin 28 //E0-DIR
+#define J2enablePin 24 // E0-EN
+#define J2limitPin 2 //X-MAX
 
-#define J3stepPin 54 // X-STEP
-#define J3dirPin 55 // X-DIR
-#define J3enablePin 38 // X-EN
+#define J3stepPin 36 //E1-STEP
+#define J3dirPin 34 //E1-DIR
+#define J3enablePin 30 // E1-EN
+#define J3limitPin 18 //Z-MIN
 
-#define J4stepPin 60 // Y-STEP
-#define J4dirPin 61 // Y-DIR
-#define J4enablePin 56 // Y-EN
+#define J4stepPin 54 //X-STEP
+#define J4dirPin 55 //X-DIR
+#define J4enablePin 38 //X-EN
+#define J4limitPin 5 //X-MIN 
 
-#define J5stepPin 10
-#define J5dirPin 11
-#define J5enablePin 24 // 
+#define J5stepPin 60 //Y-STEP
+#define J5dirPin 61 //Y-DIR
+#define J5enablePin 56 //Y-EN 
+#define J5limitPin 14 //Y-MIN
 
-#define J6stepPin 12
-#define J6dirPin 13
-#define J6enablePin 24 // 
-
-//TODO: set correct pins
-// Limit switch pin definitions
-#define J1limitPin 2   
-#define J2limitPin 3   
-#define J3limitPin 4   
-#define J4limitPin 5  
-#define J5limitPin 6  
-#define J6limitPin 7  
+#define J6stepPin 12 //Z-STEP
+#define J6dirPin 13 //Z-DIR
+#define J6enablePin 24 //Z-EN
+#define J6limitPin 15 //Y-MAX    
 
 #endif
